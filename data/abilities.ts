c37f76c85eeac8954e2ef5b9a23e3f77568f7996
TheAnimemer <146009953+TheAnimemer@users.noreply.github.com>
2025-04-06 21:28:15 -0400

    Added Signatures

--- data/abilities.ts
+++ data/abilities.ts
@@ -54,42 +54,6 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		rating: 4,
 		num: 91,
 	},
-	ironshooter: {
-		onBasePowerPriority: 19,
-		onBasePower(basePower, attacker, defender, move) {
-			if (move.flags['bullet']) {
-				return this.chainModify(1.3);
-			}
-		},
-		flags: {},
-		name: "Iron Shooter",
-		rating: 3,
-		num: -1,
-	},
-	soothingstarlight: {
-		onUpdate(pokemon) {
-			if (pokemon.volatiles['attract']) {
-				this.add('-activate', pokemon, 'ability: Soothing Starlight');
-				pokemon.removeVolatile('attract');
-				this.add('-end', pokemon, 'move: Attract', '[from] ability: Soothing Starlight');
-			}
-			if (pokemon.volatiles['taunt']) {
-				this.add('-activate', pokemon, 'ability: Soothing Starlight');
-				pokemon.removeVolatile('taunt');
-				// Taunt's volatile already sends the -end message when removed
-			}
-		},
-		onTryBoost(boost, target, source, effect) {
-			if (effect.name === 'Intimidate' && boost.atk) {
-				delete boost.atk;
-				this.add('-fail', target, 'unboost', 'Attack', '[from] ability: Scrappy', '[of] ' + target);
-			}
-		},
-		flags: {},
-		name: "Soothing Starlight",
-		rating: 3,
-		num: -2,
-	},
 	aerilate: {
 		onModifyTypePriority: -1,
 		onModifyType(move, pokemon) {
@@ -5617,7 +5581,47 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 		num: 278,
 	},
 
+	// AniCAP Abilities
+
+	ironshooter: {
+		onBasePowerPriority: 19,
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.flags['bullet']) {
+				return this.chainModify(1.3);
+			}
+		},
+		flags: {},
+		name: "Iron Shooter",
+		rating: 3,
+		num: -1,
+	},
+	soothingstarlight: {
+		onUpdate(pokemon) {
+			if (pokemon.volatiles['attract']) {
+				this.add('-activate', pokemon, 'ability: Soothing Starlight');
+				pokemon.removeVolatile('attract');
+				this.add('-end', pokemon, 'move: Attract', '[from] ability: Soothing Starlight');
+			}
+			if (pokemon.volatiles['taunt']) {
+				this.add('-activate', pokemon, 'ability: Soothing Starlight');
+				pokemon.removeVolatile('taunt');
+				// Taunt's volatile already sends the -end message when removed
+			}
+		},
+		onTryBoost(boost, target, source, effect) {
+			if (effect.name === 'Intimidate' && boost.atk) {
+				delete boost.atk;
+				this.add('-fail', target, 'unboost', 'Attack', '[from] ability: Scrappy', '[of] ' + target);
+			}
+		},
+		flags: {},
+		name: "Soothing Starlight",
+		rating: 3,
+		num: -2,
+	},
+
 	// CAP
+
 	mountaineer: {
 		onDamage(damage, target, source, effect) {
 			if (effect && effect.id === 'stealthrock') {
