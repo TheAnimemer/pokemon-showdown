c1dc41a3277c6255997ac04e39b54687694ea069
TheAnimemer <146009953+TheAnimemer@users.noreply.github.com>
2025-08-05 22:43:59 -0400

    Added "testtier" mod

---
+++ data/mods/testtier/abilities.ts
@@ -0,0 +1,6 @@
+export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
+	commander: {
+		inherit: true,
+		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, cantsuppress: 1, notransform: 1 },
+	},
+};
---
+++ data/mods/testtier/pokedex.ts
@@ -0,0 +1,8 @@
+export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
+	witheroot: {
+		inherit: true,
+		types: ["Grass", "Poison"],
+		baseStats: {hp: 95, atk: 120, def: 85, spa: 100, spd: 85, spe: 97},
+		abilities: {0: "Corrosion", H: "Toxic Debris"},
+	},
+};
