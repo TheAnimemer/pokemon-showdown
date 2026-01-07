e9c0db62ee7912069e1567f3d0c9696916e44f5d
TheAnimemer <146009953+TheAnimemer@users.noreply.github.com>
2026-01-06 22:03:25 -0500

    test tier changes

--- data/mods/testtier/pokedex.ts
+++ data/mods/testtier/pokedex.ts
@@ -1,8 +1,19 @@
 export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
 	liliamp: {
 		inherit: true,
-		types: ["Water", "Grass"],
 		baseStats: {hp: 80, atk: 75, def: 80, spa: 90, spd: 100, spe: 116},
 		abilities: {0: "Neutralizing Gas"},
+	},
+	froslassmega: {
+		inherit: true,
+		baseStats: {hp: 70, atk: 80, def: 80, spa: 130, spd: 80, spe: 137},
+	},
+	serposa: {
+		inherit: true,
+		baseStats: {hp: 79, atk: 72, def: 92, spa: 117, spd: 66, spe: 114},
+	},
+	sinester: {
+		inherit: true,
+		baseStats: {hp: 95, atk: 120, def: 95, spa: 55, spd: 115, spe: 111},
 	},
 };
