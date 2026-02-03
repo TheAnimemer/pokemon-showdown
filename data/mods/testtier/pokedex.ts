00f34d5533c14ab36cddf289c7217dc622ae783e
TheAnimemer <146009953+TheAnimemer@users.noreply.github.com>
2026-02-02 19:56:24 -0500

    speed draft v2, testing

--- data/mods/testtier/pokedex.ts
+++ data/mods/testtier/pokedex.ts
@@ -1,12 +1,7 @@
 export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable = {
-	liliamp: {
-		inherit: true,
-		baseStats: {hp: 80, atk: 75, def: 80, spa: 90, spd: 100, spe: 116},
-		abilities: {0: "Neutralizing Gas"},
-	},
 	froslassmega: {
 		inherit: true,
-		baseStats: {hp: 70, atk: 80, def: 80, spa: 130, spd: 80, spe: 137},
+		baseStats: {hp: 70, atk: 81, def: 81, spa: 130, spd: 81, spe: 137},
 	},
 	serposa: {
 		inherit: true,
@@ -15,5 +10,33 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 	sinester: {
 		inherit: true,
 		baseStats: {hp: 95, atk: 120, def: 95, spa: 55, spd: 115, spe: 111},
+	},
+	sceptiletanoso: {
+		inherit: true,
+		baseStats: {hp: 65, atk: 128, def: 80, spa: 85, spd: 68, spe: 104},
+	},
+	sceptiletanosomega: {
+		inherit: true,
+		baseStats: {hp: 65, atk: 155, def: 90, spa: 108, spd: 90, spe: 122},
+	},
+	quaquavaltanoso: {
+		inherit: true,
+		baseStats: {hp: 85, atk: 110, def: 90, spa: 83, spd: 83, spe: 74},
+	},
+	delphoxtanosomega: {
+		inherit: true,
+		baseStats: {hp: 80, atk: 88, def: 90, spa: 145, spd: 122, spe: 109},
+	},
+	diatridon: {
+		inherit: true,
+		baseStats: {hp: 100, atk: 100, def: 92, spa: 100, spd: 105, spe: 103},
+	},
+	scaldra: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 117, def: 74, spa: 86, spd: 74, spe: 114},
+	},
+	pallapad: {
+		inherit: true,
+		baseStats: {hp: 125, atk: 95, def: 72, spa: 77, spd: 85, spe: 78},
 	},
 };
